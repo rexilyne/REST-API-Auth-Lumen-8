852eadbb70dd38642f505c58c8b1f57846339006
Adrian Metanoia Gawa <tfadriangawa@gmail.com>
2022-10-01 23:47:32 +0700

    add login function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,4 +26,35 @@ class UserController extends Controller
             'message' => 'Data added successfully'
         ], 201);
     }
+
+    public function login(Request $request) {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = User::where('email', $email)->first();
+        if(!$user) {
+            return response()->json([
+                'message' => 'Email is not registered'
+            ], 401);
+        }
+
+        $isValidPassword = Hash::check($password, $user->password);
+        if(!$isValidPassword) {
+            return response()->json([
+                'message' => 'Invalid password'
+            ], 401);
+        }
+
+        $generateToken = bin2hex(random_bytes(40));
+        $user->update([
+            'token' => $generateToken
+        ]);
+
+        return response()->json($user);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ $router->get('/', function () use ($router) {
 });
 
 $router->post('/register', 'UserController@register');
+$router->post('/login', 'UserController@login');
